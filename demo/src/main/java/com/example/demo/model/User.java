6da0220b344d50555737e5cd2eb0db7c974e08c2
Luka Lattanzi <lattanziluka203@gmail.com>
2025-06-16 16:57:04 -0300

    Commit 2 16/6/2025

---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import com.example.demo.enums.UserRole;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String name;
+
+    private UserRole role;
+
+    @Lob
+    @Column(columnDefinition = "longblob")
+    private Byte[] img;
+}
